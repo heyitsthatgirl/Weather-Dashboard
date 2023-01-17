bed16873804305af0e8fe8d7fccb64fc7dd50045
hopemansfield <hopemansfield@gmail.com>
2023-01-17 14:32:10 -0500

    all future weather data is now displayed but it looks ugly

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,12 +1,9 @@
 // API key created for OpenWeather
 var APIKey = "c782320bee57f5259773bc1678ef4852";
-// variable created to access city
-
-// create a query URL
-
-// var geocodeURL = "api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + APIKey;
 
 var cityDiv = document.getElementById("displayCity");
+
+
 // target search button
 var searchBtn = document.getElementById("searchBtn");
 searchBtn.addEventListener("click", callAPI);
@@ -31,7 +28,7 @@ function callAPI() {
         var cityName = cityInfo.name;
         console.log(cityName);
         var heading = document.createElement("h1");
-        heading.innerHTML = cityName;
+        heading.innerHTML = cityName + "  " + dayjs().format("MM/DD/YYYY");
         cityDiv.appendChild(heading);
         getLocation(data);
       }
@@ -68,8 +65,6 @@ function callAPI() {
     // grabs the current temps from the weather data
     console.log("current weather: ", data.list[0].main);
     console.log("temperature in Kelvin: ", data.list[0].main.temp);
-    // console.log("min temp: ", data.list[0].main.temp_min);
-    // console.log("max temp: ", data.list[0].main.temp_max);
     var now = data.list[0].main.temp;
     // converts Kelvin into Farenheit and adds degree symbol
     var nowFar = Math.floor((now - 273.15) * 1.8 + 32) + "\xB0" + "F";
@@ -90,6 +85,97 @@ function callAPI() {
     var humidityEl = document.createElement("p");
     humidityEl.textContent = humidity + "%";
     cityDiv.appendChild(humidityEl);
+    displayFuture(data);
+  }
+
+  function displayFuture(data){
+    var future1 = document.getElementById("future1");
+    var future2 = document.getElementById("future2");
+    var future3 = document.getElementById("future3");
+    var future4 = document.getElementById("future4");
+    var future5 = document.getElementById("future5");
+
+    var date1 = document.createElement("ul");
+    var date2 = document.createElement("ul");
+    var date3 = document.createElement("ul");
+    var date4 = document.createElement("ul");
+    var date5 = document.createElement("ul");
+
+    var icon1El = document.createElement("img");
+    var icon2El = document.createElement("img");
+    var icon3El = document.createElement("img");
+    var icon4El = document.createElement("img");
+    var icon5El = document.createElement("img");
+
+    var temp1 = document.createElement("li");
+    var temp2 = document.createElement("li");
+    var temp3 = document.createElement("li");
+    var temp4 = document.createElement("li");
+    var temp5 = document.createElement("li");
+
+    var wind1 = document.createElement("li");
+    var wind2 = document.createElement("li");
+    var wind3 = document.createElement("li");
+    var wind4= document.createElement("li");
+    var wind5 = document.createElement("li");
+
+    var humid1 = document.createElement("li");
+    var humid2 = document.createElement("li");
+    var humid3 = document.createElement("li");
+    var humid4 = document.createElement("li");
+    var humid5 = document.createElement("li");
+
+    var icon1 = data.list[7].weather[0].icon;
+    var icon2 = data.list[15].weather[0].icon;
+    var icon3 = data.list[23].weather[0].icon;
+    var icon4 = data.list[31].weather[0].icon;
+    var icon5 = data.list[39].weather[0].icon;
+
+    date1.textContent = dayjs().add(1, 'day').format("MM/DD/YYYY");
+    date2.textContent = dayjs().add(2, 'day').format("MM/DD/YYYY");
+    date3.textContent = dayjs().add(3, 'day').format("MM/DD/YYYY");
+    date4.textContent = dayjs().add(4, 'day').format("MM/DD/YYYY");
+    date5.textContent = dayjs().add(5, 'day').format("MM/DD/YYYY");
+
+    icon1El.src = "https://openweathermap.org/img/wn/" + icon1 + ".png";
+    icon2El.src = "https://openweathermap.org/img/wn/" + icon2 + ".png";
+    icon3El.src = "https://openweathermap.org/img/wn/" + icon3 + ".png";
+    icon4El.src = "https://openweathermap.org/img/wn/" + icon4 + ".png";
+    icon5El.src = "https://openweathermap.org/img/wn/" + icon5 + ".png";
+
+    temp1.textContent = "Temp: " + Math.floor((data.list[7].main.temp - 273.15) * 1.8 + 32) + "\xB0" + "F";
+    temp2.textContent = "Temp: " + Math.floor((data.list[15].main.temp - 273.15) * 1.8 + 32) + "\xB0" + "F";
+    temp3.textContent = "Temp: " + Math.floor((data.list[23].main.temp - 273.15) * 1.8 + 32) + "\xB0" + "F";
+    temp4.textContent = "Temp: " + Math.floor((data.list[31].main.temp - 273.15) * 1.8 + 32) + "\xB0" + "F";
+    temp5.textContent = "Temp: " + Math.floor((data.list[39].main.temp - 273.15) * 1.8 + 32) + "\xB0" + "F";
+
+    wind1.textContent = "Wind Speed: " + data.list[7].wind.speed + "mph";
+    wind2.textContent = "Wind Speed: " + data.list[15].wind.speed + "mph";
+    wind3.textContent = "Wind Speed: " + data.list[23].wind.speed + "mph";
+    wind4.textContent = "Wind Speed: " + data.list[31].wind.speed + "mph";
+    wind5.textContent = "Wind Speed: " + data.list[39].wind.speed + "mph";
+
+    humid1.textContent = "Humidity: " + data.list[7].main.humidity + "%";
+    humid2.textContent = "Humidity: " + data.list[15].main.humidity + "%"; 
+    humid3.textContent = "Humidity: " + data.list[23].main.humidity + "%"; 
+    humid4.textContent = "Humidity: " + data.list[31].main.humidity + "%"; 
+    humid5.textContent = "Humidity: " + data.list[39].main.humidity + "%";  
+
+    future1.appendChild(date1);
+    date1.append(icon1El, temp1, wind1, humid1);
+
+    future2.appendChild(date2);
+    date2.append(icon2El, temp2, wind2, humid2);
+
+    future3.appendChild(date3);
+    date3.append(icon3El, temp3, wind3, humid3);
+
+    future4.appendChild(date4);
+    date4.append(icon4El, temp4, wind4, humid4);
+
+    future5.appendChild(date5);
+    date5.append(icon5El, temp5, wind5, humid5);
+
   }
 }
 // need to display city name, date and icon representing current weather conditions
